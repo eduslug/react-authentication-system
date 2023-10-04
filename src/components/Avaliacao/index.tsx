9ac5ac28a5db0c3b3b02e290684ecbf932711bc3
eduslug <eduslug93@gmail.com>
2023-10-04 18:42:36 -0300

    fix/usuario-data

--- src/components/Avaliacao/index.tsx
+++ src/components/Avaliacao/index.tsx
@@ -14,11 +14,15 @@ function Avaliacao({ profissionais }: { profissionais: IProfissional[] | null })
     return (
         <>
             <SecaoCard>
-                {profissionais?.map((profissional) => {
-                    return <Card profissional={profissional} />
-                })}
+                {profissionais && profissionais.length ?
+                    profissionais?.map((profissional) => {
+                        return <Card profissional={profissional} />
+                    }) : 'nao ha avaliaçẽs para mostrar'}
             </SecaoCard>
-            <Botao>Ver mais</Botao>
+            {
+                profissionais && profissionais.length >= 4 ?
+                    <Botao>Ver mais</Botao> : ''
+            }
         </>
     )
 }
