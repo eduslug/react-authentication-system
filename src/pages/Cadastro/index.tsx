9844a2854ea862599a8e316805e8116351d863da
eduslug <eduslug93@gmail.com>
2023-10-13 00:16:38 -0300

    fix/login/cadastro-usuario

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,11 +1,10 @@
-
 import styled from 'styled-components';
-import imagem from './logo.png'
+import imagem from './logo.png';
 import { Step, StepLabel, Stepper } from '@mui/material';
 import React, { useState } from 'react';
 import CampoDigitacao from '../../components/CampoInput';
 import Botao from '../../components/Botao';
-
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Imagem = styled.img`
   padding: 2em 0;
@@ -14,15 +13,15 @@ const Imagem = styled.img`
 const Titulo = styled.h2`
   font-weight: 700;
   font-size: 24px;
-  line-height: 28px;    
-  color: var(--cinza)
+  line-height: 28px;
+  color: var(--cinza);
 `;
 
 const Paragrafo = styled.p`
   font-weight: 400;
   font-size: 20px;
   line-height: 19px;
-  color: var(--azul-escuro)
+  color: var(--azul-escuro);
 `;
 
 const ParagrafoCadastro = styled(Paragrafo)`
@@ -30,60 +29,109 @@ const ParagrafoCadastro = styled(Paragrafo)`
 `;
 
 interface PropsCustomized {
-    cor: string,
-
+    cor: string;
 }
 
 const StepCustomized = styled.div<PropsCustomized>`
-    background-color: ${({ cor }) => cor};
-    width: 16px;
-    height: 16px;
-    border-radius: 50%;
-    `
+  background-color: ${({ cor }) => cor};
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+`;
+
 const Formulario = styled.form`
   width: 70%;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
+
 const BotaoCustomizado = styled(Botao)`
   width: 50%;
   cursor: pointer;
 `;
+
+interface Sign {
+    event: string
+}
+
+
 export default function Cadastro() {
-    const [nome, setNome] = useState('')
-    const [cnpj, setCnpj] = useState('')
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
-    const [repita, setRepita] = useState('')
+    const [nome, setNome] = useState('');
+    const [cnpj, setCnpj] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [repita, setRepita] = useState('');
+
+    const navigate = useNavigate(); // Corrected the useNavigate usage
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault();
+        // You can add form submission logic here
+        // For example, redirect to another page after submission
+        navigate('/login');
+    };
+
     return (
-
-
         <>
             <Imagem src={imagem} alt='logo' />
             <Stepper>
                 <Step>
-                    <StepLabel StepIconComponent={(props) => (
-                        <StepCustomized cor={props.active ? 'lightBlue' : 'lightGreen'} />)}>
-                    </StepLabel>
+                    <StepLabel
+                        StepIconComponent={(props) => (
+                            <StepCustomized cor={props.active ? 'lightBlue' : 'lightGreen'} />
+                        )}
+                    ></StepLabel>
                 </Step>
                 <Step>
-                    <StepLabel StepIconComponent={(props) => (
-                        <StepCustomized cor={props.active ? 'lightBlue' : 'lightGreen'} />)}>
-                    </StepLabel>
+                    <StepLabel
+                        StepIconComponent={(props) => (
+                            <StepCustomized cor={props.active ? 'lightBlue' : 'lightGreen'} />
+                        )}
+                    ></StepLabel>
                 </Step>
             </Stepper>
             <>
                 <Titulo>Primeiro, alguns dados básicos:</Titulo>
-                <Formulario>
-                    <CampoDigitacao tipo="nome" label="nome" valor={nome} onChange={setNome} placeholder='Digite seu nome' />
-                    <CampoDigitacao tipo="cnpj" label="Cnpj" valor={cnpj} onChange={setCnpj} placeholder='Digite seu nome' />
-                    <CampoDigitacao tipo="email" label="email" valor={email} onChange={setEmail} placeholder='Digite seu nome' />
-                    <CampoDigitacao tipo="senha" label="senha" valor={senha} onChange={setSenha} placeholder='Digite seu nome' />
-                    <CampoDigitacao tipo="repita" label="Repita" valor={repita} onChange={setRepita} placeholder='Digite seu nome' />
+                <Formulario onSubmit={handleSubmit}>
+                    <CampoDigitacao
+                        tipo='text'
+                        label='nome'
+                        valor={nome}
+                        onChange={setNome}
+                        placeholder='Digite seu nome'
+                    />
+                    <CampoDigitacao
+                        tipo='text'
+                        label='Cnpj'
+                        valor={cnpj}
+                        onChange={setCnpj}
+                        placeholder='Digite seu nome'
+                    />
+                    <CampoDigitacao
+                        tipo='email'
+                        label='email'
+                        valor={email}
+                        onChange={setEmail}
+                        placeholder='Digite seu nome'
+                    />
+                    <CampoDigitacao
+                        tipo='password'
+                        label='senha'
+                        valor={senha}
+                        onChange={setSenha}
+                        placeholder='Digite seu nome'
+                    />
+                    <CampoDigitacao
+                        tipo='password'
+                        label='Repita'
+                        valor={repita}
+                        onChange={setRepita}
+                        placeholder='Digite seu nome'
+                    />
                     <BotaoCustomizado type='submit'>Avançar</BotaoCustomizado>
                 </Formulario>
             </>
         </>
-    )
+    );
 }
